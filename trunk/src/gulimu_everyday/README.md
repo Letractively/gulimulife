5d29e9ad8dd2b90ae9b6968c46b9fbb35710d61f
lucienzhu@gmail.com <lucienzhu@gmail.com@acdc308a-8d4a-d0ef-7082-d3e89579d4a6>
2011-09-01 13:12:08 +0000

    ADD: everyday; MOD: travelmap; DEL: readbook

---
+++ trunk/src/gulimu_everyday/README.md
@@ -0,0 +1,19 @@
+GulimuLife Everyday
+=======
+
+Introduction
+-------
+This app is to monitor the time spent on different purpose.
+It is a life-streaming app for [Google Calendar](http://calendar.google.com "Calendar").
+
+Input Format
+-------
+In your calendar, you should have a data entry like this.
+
+> WhatYouDo||MOOD$$TAG1,TAG2&&
+
+The Syntax is yet to be decided.
+
+Output
+-------
+On the client side, it will be presented as a dash board.
---
+++ trunk/src/gulimu_everyday/everyday.py
@@ -0,0 +1,57 @@
+'''
+Created on 2011-09-01
+
+@author: gulimujyujyu
+'''
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import os
+
+from google.appengine.api import users
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+from utils import constants
+
+##Globals
+everyday_prefix = '/everyday'
+
+#main page
+class MainPage(webapp.RequestHandler):
+    title = 'Main Page'
+
+    def get(self):
+        if not users.get_current_user():
+            self.redirect(users.create_login_url(self.request.uri));
+
+        template_values = {
+            'user': users.get_current_user(),
+        }
+
+        path = os.path.join(constants.Constants.TEMPLATE_PATH, 'everyday/everyday.html')
+        self.response.out.write(template.render(path, template_values));
+
+    def post(self):
+        self.get();
+
+#404 page
+class ErrorPage(webapp.RequestHandler):
+
+    def get(self):
+        path = os.path.join(constants.Constants.TEMPLATE_PATH, '404.html');
+
+        template_dict = {
+            "error_source": "Everyday"};
+        self.response.out.write(template.render(path, template_dict));
+
+#manage links
+def main():
+    application = webapp.WSGIApplication([(everyday_prefix, MainPage),
+                                          (everyday_prefix + '.*', ErrorPage)],
+                                         debug=True)
+    run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
